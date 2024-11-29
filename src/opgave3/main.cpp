15edefdffba147b63e2dbb20b19e8a59a75506b0
Brian Vestergaard Danielsen <0x627664@gigaslack.com>
2024-11-29 09:52:29 +0100

    Remove local file

--- src/opgave3/main.cpp
+++ src/opgave3/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "Medarbejder.h"
 
 int main(int argc, char** argv) {
-  std::cout << "Opgave 3" << std::endl;
+  
+  std::cout << Medarbejder().getNavn() << std::endl;
+  std::cout << Medarbejder("John Doe").getNavn() << std::endl;
+
   return 0;
 }
